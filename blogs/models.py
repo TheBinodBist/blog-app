4a5564cbf6fcbe82336c0d5a17e3423da9fed151
Binod Bist <thebinodbist@gmail.com>
2025-12-16 18:35:12 +0545

    print category in home page

--- blogs/models.py
+++ blogs/models.py
@@ -14,22 +14,22 @@ class Category(models.Model):
         return self.category_name
 
 STATUS_CHOICES =(
-    (0,"Draft"),
-    (1, "Published")
+    ("Draft","Draft"),
+    ("Published", "Published")
 )  
 
-class  Blog(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=150, unique=True)
-    Category  = models.ForeignKey(Category, on_delete=models.CASCADE)
-    author= models.ForeignKey(User, on_delete= models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
-    short_description= models.TextField(max_length=500)
+    short_description = models.TextField(max_length=500)
     blog_body = models.TextField(max_length=2000)
-    status = models.IntegerField(choices=STATUS_CHOICES,default=0)
-    is_Featured= models.BooleanField(default=False)
-    created_at= models.DateTimeField(auto_now_add=True)
-    updated_at= models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=24,choices=STATUS_CHOICES, default="Draft")
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
